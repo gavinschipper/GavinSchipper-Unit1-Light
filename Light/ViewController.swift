2ee80d0725f1ade3dfdcbc2e88cead9776fd69bf
Gavin Schipper <gavin_schipper@hotmail.com>
2017-11-02 12:02:17 +0100

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Gavin Schipper on 02-11-17.
+//  Copyright © 2017 Gavin Schipper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
